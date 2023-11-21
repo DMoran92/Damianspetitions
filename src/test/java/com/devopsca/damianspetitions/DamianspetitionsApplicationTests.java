c0b42c81e3bf01faf8ecd596a24d93dd92531112
DMoran92 <d.moraniec1@universityofgalway.ie>
2023-11-21 17:34:40 +0000

    initial commit with hello world

---
+++ src/test/java/com/devopsca/damianspetitions/DamianspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devopsca.damianspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamianspetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
