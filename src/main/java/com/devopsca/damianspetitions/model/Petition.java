19dd19f5846089ee140523cac179c1aa2118bf8c
DMoran92 <d.moraniec1@universityofgalway.ie>
2023-11-22 17:42:27 +0000

    initial models for Petitions and Signatures
    Each petition can have multiple signatures assigned, that's why signatures are separate DTO.

---
+++ src/main/java/com/devopsca/damianspetitions/model/Petition.java
@@ -0,0 +1,50 @@
+package com.devopsca.damianspetitions.model;
+
+import java.util.List;
+
+public class Petition {
+    private Long id;
+    private String title;
+    private String content;
+    private List<Signature> signatures;
+
+    public Petition() {
+    }
+
+    public Petition(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<Signature> getSignatures() {
+        return signatures;
+    }
+
+    public void setSignatures(List<Signature> signatures) {
+        this.signatures = signatures;
+    }
+}
